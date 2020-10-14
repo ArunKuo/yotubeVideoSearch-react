df39a67683d566f8a30c39f2faf4b781dc8264f5
ArunKuo <steins1119@gmail.com>
2020-10-14 14:14:02 +0800

    Updates

---
+++ precache-manifest.1dc109f9bb6b4176b953277889c35bcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f6b06e30fefcfa823969fb8c5e8cdc",
+    "url": "/yotubeVideoSearch-react/index.html"
+  },
+  {
+    "revision": "f32163cb15c1663acd8d",
+    "url": "/yotubeVideoSearch-react/static/css/main.6054b9ec.chunk.css"
+  },
+  {
+    "revision": "a0aa47ee8134dbc31c96",
+    "url": "/yotubeVideoSearch-react/static/js/2.073fe359.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yotubeVideoSearch-react/static/js/2.073fe359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f32163cb15c1663acd8d",
+    "url": "/yotubeVideoSearch-react/static/js/main.9e32a93c.chunk.js"
+  },
+  {
+    "revision": "8733d06a86348010d107",
+    "url": "/yotubeVideoSearch-react/static/js/runtime-main.419ef328.js"
+  }
+]);
